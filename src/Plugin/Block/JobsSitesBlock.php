757089a6c7d76d2399a80adc735ba9f72309a223
rmedard <00Derfine@5>
2018-07-14 22:58:25 +0200

    W10

---
+++ src/Plugin/Block/JobsSitesBlock.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medar
+ * Date: 14/07/2018
+ * Time: 22:52
+ */
+
+namespace Drupal\jir_blocks\Plugin\Block;
+
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Class JobsSitesBlock
+ * @package Drupal\jir_blocks\Plugin\Block
+ * @Block(
+ *     id = "jobs_sites_block",
+ *     admin_label = @Translation("Jobs Sites block"),
+ *     category = @Translation("Custom JIR Blocks")
+ * )
+ */
+class JobsSitesBlock extends BlockBase
+{
+
+    /**
+     * Builds and returns the renderable array for this block plugin.
+     *
+     * If a block should not be rendered because it has no content, then this
+     * method must also ensure to return no content: it must then only return an
+     * empty array, or an empty array with #cache set (with cacheability metadata
+     * indicating the circumstances for it being empty).
+     *
+     * @return array
+     *   A renderable array representing the content of the block.
+     *
+     * @see \Drupal\block\BlockViewBuilder
+     */
+    public function build()
+    {
+        return[
+            '#theme' => 'jix_jobs_sites',
+        ];
+    }
+}
